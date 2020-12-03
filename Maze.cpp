d1e8b881fd3c2622bbeadb2b30c5bc09a5388d99
Andrew Lopez <andrewlopez@ufl.edu>
2020-12-03 14:41:38 -0500

    Gneral outline for maze

--- Maze.cpp
+++ Maze.cpp
@@ -1,11 +1,19 @@
 #include "Maze.h"
 
-Maze::Node::Node()
+Maze::Maze()
 {
-
+	width = 9;
+	height = 9;
+	//creates default maze as a line
+	theMaze[1][0] = 2;
+	theMaze[1][8] = 2;
+	for (int i = 1; i < 8; i++)
+	{
+		theMaze[1][i] = 1;
+	}
 }
 
-Maze::Maze()
+void Maze::GenerateMaze(int width, int height)
 {
 
 }
--- Maze.h
+++ Maze.h
@@ -6,20 +6,16 @@ using namespace std;
 
 class Maze
 {
-	struct Node
-	{
-		bool isStart = false;
-		bool isEnd = false;
-		bool isObj = false;
-		vector<Node*> adjacent;
-
-		Node();
-	};
-
-	Node theMaze[500][500];
+	//Maze where 0 represents a wall, 1 represents a path, 2 represents the start/end
+	vector<vector<int>> theMaze;
+	int width = 0;
+	int height = 0;
 
 public:
 	Maze();
+	void setVertex(int x, int y, int val);
+	int getVertex(int x, int y);
+	void GenerateMaze(int width, int height);
 	int Djikstra();
 	int DFS();
 };
